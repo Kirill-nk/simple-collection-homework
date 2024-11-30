06c8172a0b7007f839b17f4391792436db433764
Kirill-nk <kirill9414@yandex.ru>
2024-11-30 22:57:04 +0300

    Начало TodoList

--- homework-01-collections-list/src/main/java/com/company/Main.java
+++ homework-01-collections-list/src/main/java/com/company/Main.java
@@ -1,7 +1,18 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         // TODO: написать консольное приложение для работы со списком дел todoList
+        System.out.println("Введите одну из команд: LIST, ADD, EDIT, DELETE");
+        Scanner scanner = new Scanner(System.in);
+        TodoList todoList =new TodoList();
+        while (true){
+            String input = scanner.nextLine();
+            if (input.equals("LIST")) {
+                System.out.println(todoList.getTodos());
+            }
+        }
     }
 }
